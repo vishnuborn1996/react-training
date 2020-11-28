51c269f5008b967503a5361b9d8a96d38bfdffe3
Vishnu Bhaskaran <33519729+vishnuborn1996@users.noreply.github.com>
2020-11-29 00:56:59 +0530

    added next tasks

--- src/pages/courses/CourseList.jsx
+++ src/pages/courses/CourseList.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+
+/*
+**  create a table structure for displaying the course
+ignore the next..
+**  in the table try to add a button named "view"
+**  on clicking this button open a different page called CourseDetails and show all the course related info
+**  for this u need to use link of router  
+*/
+
+/*
+   just like how we created course, create a form for adding new author;
+   
+*/
+
 function CourseList() {
     let courses = JSON.parse(localStorage.getItem("courses"));
 
