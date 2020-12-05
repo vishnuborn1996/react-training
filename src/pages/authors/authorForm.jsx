b0738fc5098b12711a6f16a3868f6673b3f0d9a2
Vishnu Bhaskaran <33519729+vishnuborn1996@users.noreply.github.com>
2020-12-05 15:15:40 +0530

    fixed bugs

--- src/pages/authors/authorForm.jsx
+++ src/pages/authors/authorForm.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-function AuthorForm(props){
+function AuthorForm(props) {
 
-    return(
+    return (
 
         <div>
             <form onSubmit={props.onSubmitHandler} action="POST">
                 <h3>Manage Author</h3>
                 <div>
                     <label>Author Name</label>
-                    <input type="text" name= "authorName" onChange={props.onChangeHandler}> </input>
+                    <input type="text" name="authorName" onChange={props.onChangeHandler} />
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="email" name="email" onChange={props.onChangeHandler}></input>
+                    <input type="email" name="email" onChange={props.onChangeHandler} />
                 </div>
                 <div>
                     <button type="submit" onSubmit={props.onSubmitHandler}>Submit</button>
--- src/pages/authors/manageAuthors.jsx
+++ src/pages/authors/manageAuthors.jsx
@@ -2,44 +2,43 @@ import React, { useState } from "react";
 import History from '../../settings/History';
 import AuthorForm from "./authorForm";
 
-function ManageAuthors(){
+function ManageAuthors() {
 
     const initialState = { authorName: "", email: "" };
-    const [authorForm,setAuthorForm]= useState(initialState)
+    const [authorForm, setAuthorForm] = useState(initialState)
 
-    function onChangeHandler({target}){
-         
-        setAuthorForm({...authorForm, [target.name]: target.value});
+    function onChangeHandler({ target }) {
+
+        setAuthorForm({ ...authorForm, [target.name]: target.value });
     }
 
-    function onSubmitHandler(event){
+    function onSubmitHandler(event) {
 
         event.preventDefault();
-        let formValid= true;
+        let formValid = true;
 
-        if (authorForm.authorName===""){
-            formValid= false;
+        if (authorForm.authorName === "") {
+            formValid = false;
         }
-        if (authorForm.email===""){
-            formValid= false;
+        if (authorForm.email === "") {
+            formValid = false;
         }
-        if(formValid){
-            let savedAuthors= JSON.parse(localStorage.getItem("authors"));
+        if (formValid) {
+            let savedAuthors = JSON.parse(localStorage.getItem("authors"));
             if (savedAuthors == null) {
                 savedAuthors = [];
             }
 
             savedAuthors.push(authorForm);
 
-            localStorage.setItem("authors",JSON.stringify(savedAuthors));
+            localStorage.setItem("authors", JSON.stringify(savedAuthors));
         }
 
     }
 
     return (
         <>
-        
-            <authorForm onChangeHandler={onChangeHandler} onSubmitHandler={onSubmitHandler} />
+            <AuthorForm onChangeHandler={onChangeHandler} onSubmitHandler={onSubmitHandler} />
         </>
     );
 }
